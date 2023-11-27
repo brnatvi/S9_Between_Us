71d7e4113592bee170a8be42d6886d9604f3e113
MESSIKH Abdennour <messikh.abdennour@gmail.com>
2023-11-27 14:27:57 +0100

    touching up small details for REST requesets/interactions

--- client.go
+++ client.go
@@ -56,7 +56,7 @@ type message struct {
 
 // debug variable, set true to enable debug fmt.prints
 const debug = true
-const url = "/"
+const url = "https://jch.irif.fr:8443/"
 
 // var id = 0 // an incrementing counter for the id field
 
@@ -96,21 +96,22 @@ Options:
 		cmd = cmd[:len(cmd)-1]
 		CheckErr(err, "Read err!")
 
-		switch parseCmd(cmd) {
+		command, peer := parseCmd(cmd)
+		switch command {
 		case 0:
-			// getPeers(client)
+			getPeers(client)
 			DebugPrint("get peers")
 		case 1:
-			// peerAddr(client, peer)
+			peerAddr(client, peer)
 			DebugPrint("addr")
 		case 2:
-			// peerKey(client, peer)
+			peerKey(client, peer)
 			DebugPrint("key")
 		case 3:
-			// peerRoot(client, peer)
+			peerRoot(client, peer)
 			DebugPrint("root")
 		case 4:
-			// getData(peer)
+			getData(peer)
 			DebugPrint("data")
 		case 5:
 			return
@@ -120,29 +121,29 @@ Options:
 	}
 }
 
-func parseCmd(cmd string) (ret int) {
+func parseCmd(cmd string) (ret int, peer string) {
 	// TODO, code commands to ints 0-5 then return said commands + extra args if necessary
 	split := strings.Split(cmd, "-")
+	split[0] = strings.TrimSpace(split[0])
 	DebugPrint(split[0])
-
 	switch split[0] {
 	case "list":
-		return 0
+		return 0, ""
 	case "exit":
-		return 5
+		return 5, ""
 	default:
 		switch split[1] {
 		case "a":
-			return 1
+			return 1, split[0]
 		case "k":
-			return 2
+			return 2, split[0]
 		case "r":
-			return 3
+			return 3, split[0]
 		case "d":
-			return 4
+			return 4, split[0]
 		}
 	}
-	return -1
+	return -1, ""
 }
 
 // TODO directory/file ==> merkel tree
@@ -157,13 +158,15 @@ func merkelify(path string) (root node) {
 }
 
 func hashDir(path string) (root node) {
-	dir, err := os.ReadDir(path)
+	_, err := os.ReadDir(path)
 	CheckErr(err, "os.readdir err, hashDir")
+	return
 }
 
 func hashFile(path string) (root node) {
-	fi, err := os.Stat(path)
+	_, err := os.Stat(path)
 	CheckErr(err, "os.stat err, hashFile")
+	return
 }
 
 func getUrl(tcpClient *http.Client, ReqUrl string) (*http.Response, error) {
@@ -173,32 +176,43 @@ func getUrl(tcpClient *http.Client, ReqUrl string) (*http.Response, error) {
 
 	res, err := tcpClient.Do(req)
 
+	return res, err
+
 }
 
 func getPeers(tcpClient *http.Client) {
 
-	res, err := getUrl(tcpClient, url+"/peers")
+	res, err := getUrl(tcpClient, url+"peers")
 	CheckErr(err, "get error /peers")
 	// TODO format the addresses nicely before return
+	DebugPrint(res.Body)
 }
 
 func peerAddr(tcpClient *http.Client, peer string) {
 
-	res, err := getUrl(tcpClient, url+"/peers"+peer+"addresses")
+	res, err := getUrl(tcpClient, url+"peers/"+peer+"/addresses")
+	DebugPrint(url + "peers/" + peer + "/addresses")
 	CheckErr(err, "get error /peers/p/addresses")
 	// TODO jp (just print)
+	p := make([]byte, 200)
+	res.Body.Read(p)
+	DebugPrint(p)
+
 }
 
 func peerKey(tcpClient *http.Client, peer string) {
-	res, err := getUrl(tcpClient, url+"/peers"+peer+"addresses")
+	res, err := getUrl(tcpClient, url+"peers/"+peer+"/key")
 	CheckErr(err, "get error /peers/p/key")
 	// TODO jp
+	DebugPrint(res.Body)
+
 }
 
 func peerRoot(tcpClient *http.Client, peer string) {
-	res, err := getUrl(tcpClient, url+"/peers"+peer+"addresses")
+	res, err := getUrl(tcpClient, url+"peers/"+peer+"/root")
 	CheckErr(err, "get error /peers/p/root")
 	// TODO jp
+	DebugPrint(res.Body)
 }
 
 // getDatum req
@@ -219,6 +233,6 @@ func CheckErr(err error, msg string) {
 
 func DebugPrint(msg interface{}) {
 	if debug {
-		fmt.Printf("%q\n", msg)
+		fmt.Printf("%q \n", msg)
 	}
 }
